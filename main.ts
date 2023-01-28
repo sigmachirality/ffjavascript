6d37498fb5e1576b0b2d65c0b8c3f6bccfd96ea9
Daniel Tao <danieltaox@gmail.com>
2023-01-27 19:37:11 -0500

    feat: update types for main

---
+++ main.ts
@@ -0,0 +1,22 @@
+
+export * as Scalar from "./src/scalar";
+
+export {default as PolField} from "./src/polfield";
+export {default as F1Field} from "./src/f1field";
+export {default as F2Field} from "./src/f2field";
+export {default as F3Field} from "./src/f3field";
+
+export {default as ZqField} from "./src/f1field";
+
+export {default as EC} from "./src/ec";
+
+export {default as buildBn128} from "./src/bn128";
+export {default as buildBls12381} from "./src/bls12381";
+
+export * as utils from "./src/utils.js";
+export {default as ChaCha} from "./src/chacha.js";
+
+export {default as BigBuffer} from "./src/bigbuffer.js";
+
+export {getCurveFromR, getCurveFromQ, getCurveFromName} from "./src/curves.js";
+
